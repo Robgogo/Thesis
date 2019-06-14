405142767efc3258668eda50ea1926e0d4192b46
Robgogo <Robgogoworku@gmail.com>
2019-06-14 11:21:58 +0300

    Arduino Files Changed

--- Pro/New folder/pro/GNode/GNode.ino
+++ Pro/New folder/pro/GNode/GNode.ino
@@ -1,186 +1,156 @@
-#include"RTClib.h"
-#include <Wire.h>
+#include <SoftwareSerial.h>
 
-#include<SoftwareSerial.h>
+SoftwareSerial xbee(8,7);
+SoftwareSerial mySerial(9, 10);
 
-
-
-RTC_DS1307 rtc;
-int echoPin=11;
-int  trigPin=13;
-float pingTime;
-float targetDistance;
-float originalDist=2;
-float level;
-float speedOfSound=343;//in meter per second
-String data=  "";
-int sensorInterrupt=0;
-int flowRatePin=2;
-float calibrationFactor=4.5;
-int pulseCount;
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
-unsigned long oldTime;
-
-int OnOffLed=6;
-int statusGreen=8;
-int statusRed=7;
-int brightness=100;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-SoftwareSerial mySerial(9,10);
-
+//Variables
+bool started= false;//True: Message is strated
+bool ended  = false;//True: Message is finished 
+char incomingByte ; //Variable to store the incoming byte
+char msg[200];    //Message - array from 0 to 2 (3 values - PWM - e.g. 240)
+byte index;     //Index of array
+String testReading="";
+String dummyData="\"Sensor 2\":{\"flowRate\":23,\"level\":1.56,\"timeOfReading\":\"2019-06-07T14:14:56\"}";
 void setup() {
-  mySerial.begin(9600);
-  Serial.begin(9600);
-  pinMode(echoPin,INPUT);
-  pinMode(trigPin,OUTPUT);
-  pinMode(flowRatePin, INPUT);
-  digitalWrite(flowRatePin, HIGH);
-  pinMode(OnOffLed,OUTPUT);
-  pinMode(statusGreen,OUTPUT);
-  pinMode(statusRed,OUTPUT);
-
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
+  // put your setup code here, to run once:
+  mySerial.begin(115200);
+  xbee.begin(9600);
+  Serial.begin(9600); //Baud rate must be the same as is on xBeinte module
+  Serial.println("Recieving");
+  
   delay(100);
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-  attachInterrupt(sensorInterrupt,pulseCounter,FALLING);
-  mySerial.println("AT");
+//  mySerial.println("AT");
+  
 
 }
 
 void loop() {
 
+  Serial.println("Waiting");
+  xbee.listen();
+  // put your main code here, to run repeatedly:
+  while (xbee.available()>0){
+//    Serial.println("xbee available");
+    //Read the incoming byte
+    incomingByte = xbee.read();
+    //Start the message when the '<' symbol is received
+    Serial.print(incomingByte);
+    if(incomingByte == '<')
+    {
+     started = true;
+     index = 0;
+     msg[index] = '\0'; // Throw away any incomplete packet
+   }
+   //End the message when the '>' symbol is received
+   else if(incomingByte == '>')
+   {
+     ended = true;
+     break; // Done reading - exit from while loop!
+   }
+   //Read the message!
+   else
+   {
+     if(index < 201) // Make sure there is room
+     {
+       msg[index] = incomingByte; // Add char to array
+       index++;
+       msg[index] = '\0'; // Add NULL to end
+     }
+   }
+ }
+ 
+ if(started && ended)
+ {
+   String value(msg);
+   Serial.println("Recieved");
+    //Only for debugging
+   index = 0;
+   msg[index] = '\0';
+   started = false;
+   ended = false;
+   testReading="{"+value+",\"timeOfReading\":\"2019-06-07T14:14:56\"},"+dummyData+"}";
+   Serial.print(testReading);
+   Serial.println();
+   mySerial.listen();
+   connectGPRS();
+   connectHTTP();
+   
+ }
+ if(mySerial.available()>0){
+  Serial.write(mySerial.read());
+ }
+delay(15000);
 
-  digitalWrite(OnOffLed,HIGH);
-  digitalWrite(trigPin,LOW);
-  delayMicroseconds(2000);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(15);
-  digitalWrite(trigPin,LOW);
+}
 
-  pingTime=pulseIn(echoPin,HIGH);
-  pingTime=pingTime/1000000.;
-
-  targetDistance=speedOfSound * pingTime;
-  targetDistance=targetDistance/2.;
-
-  level=originalDist - targetDistance;
-
-//  Serial.print("The distance is: ");
-//  Serial.println(level);
-
-  
-//  delay(1000);
-  
-  if((millis()-oldTime)>1000){
-    detachInterrupt(sensorInterrupt);
-    flowRate= ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    oldTime = millis();
-    flowMilliLitres = (flowRate / 60) * 1000;
-    totalMilliLitres += flowMilliLitres;
-    unsigned int frac;
-//    Serial.print("Flow rate: ");
-//    Serial.print(int(flowRate));  // Print the integer part of the variable
-//    Serial.print(".");             // Print the decimal point
-    frac = (flowRate - int(flowRate)) * 10;
-//    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-//    Serial.print("L/min");
-//    Serial.print("  Current Liquid Flowing: ");             // Output separator
-//    Serial.print(flowMilliLitres);
-//    Serial.print("mL/Sec");
-//    Serial.print("  Output Liquid Quantity: ");             // Output separator
-//    Serial.print(totalMilliLitres);
-//    Serial.println("mL"); 
-    pulseCount = 0;
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  }
-
-  
-  DateTime now=rtc.now();
-//  Serial.print(now.year(), DEC);
-//  Serial.print('/');
-//  Serial.print(now.month(), DEC);
-//  Serial.print('/');
-//  Serial.print(now.day(), DEC);
-//  Serial.print(" (");
-//  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-//  Serial.print(") ");
-//  Serial.print(now.hour(), DEC);
-//  Serial.print(':');
-//  Serial.print(now.minute(), DEC);
-//  Serial.print(':');
-//  Serial.print(now.second(), DEC);
-//  Serial.println();
-  String timeOfRecord=String(now.year(), DEC)+'/'+String(now.month(), DEC)+'/'+String(now.day(), DEC)+" "+String(now.hour(), DEC)+":"+String(now.minute(), DEC)+":"+String(now.second(), DEC);
-  
-  String gnodeData="\"sensor2\":{\"flowrate\":"+(String)flowRate+",\"level\":"+(String)level+",\"time\":"+timeOfRecord+"}";
-  if(Serial.available()>0){
-    Serial.println("Reading data...");
-    String nodeData=Serial.readString();
-    delay(800);
-    //Serial.println(nodeData);
-    data=data+gnodeData+","+nodeData;
-    digitalWrite(statusGreen,HIGH);
-    SendMessage(data);
-    digitalWrite(statusGreen,LOW);
-  }
-
-  if(mySerial.available()){
+void ShowSerialData() {
+  while(mySerial.available()!=0)
     Serial.write(mySerial.read());
-  }
-
-  delay(500);
-
 }
 
-void SendMessage(String data){
-  mySerial.println("AT+HTTPINIT");
-  mySerial.println((char)13);
-  delay(500);
-  mySerial.println("AT+HTTPPARA=\"CID\",1");
-  delay(500);
-  mySerial.println("AT+HTTPPARA=\"URL\",\"http://localhost:8080/api/add\"");
-  delay(500);
-  mySerial.println("AT+HTTPPARA=\"CONTENT\",\"multipart/form-data; boundary=----WebKitFormBoundaryvZ0ZHShNAcBABWFy\"");
-  delay(500);
-  mySerial.println("AT+HTTPDATA=192,10000");
+void connectGPRS()
+{ 
+  mySerial.println("AT+CGATT=1");
+  delay(100);
+  ShowSerialData();
+  mySerial.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
+  delay(100);
+  ShowSerialData();
 
-  mySerial.println("------WebKitFormBoundaryvZ0ZHShNAcBABWFy");
-  mySerial.println("Content-Disposition: form-data; name=\"fileToUpload\"; filename=\"data.json\"");
-  mySerial.println("Content-Type: application/json");
-  //String data="{\"name\":\"Kiyaa\",\"location\":\"adama\",\"level\":"+(String)targetDistance+",\"flowRate\":"+(String)flowRate+"}";
-  mySerial.println(data);
-  mySerial.println("------WebKitFormBoundaryvZ0ZHShNAcBABWFy");
-  mySerial.println("AT+HTTPACTION=1");
-  delay(500);
+  mySerial.println("AT+SAPBR=3,1,\"APN\",\"etc.com\"");//APN
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+SAPBR=1,1");
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+SAPBR=2,1");
+  delay(100);
+  ShowSerialData();
 }
 
-void RecieveMessage(){
-  mySerial.println("AT+CNMI=2,2,0,0,0");
-  delay(1000);
-}
-
-void pulseCounter()
+void connectHTTP()
 {
-  // Increment the pulse counter
-  pulseCount++;
+  mySerial.println("AT+HTTPTERM");
+  delay(100);
+  ShowSerialData();
+  mySerial.println("AT+HTTPINIT");
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+HTTPPARA=\"CID\",1");
+  delay(100);
+  ShowSerialData();
+
+
+  mySerial.println("AT+HTTPPARA=\"URL\",\"http://vast-eyrie-51209.herokuapp.com/api/postdata\"");//Public server address
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+HTTPPARA=\"CONTENT\",\"application/json\"");
+  delay(100);
+  ShowSerialData();
+
+
+  mySerial.println("AT+HTTPDATA=" + String(testReading.length()) + ",10000");
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println(testReading);
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+HTTPACTION=1");
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+HTTPREAD");
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+HTTPTERM");
+  delay(100);
+  ShowSerialData();
 }
--- Pro/New folder/pro/Node/Node.ino
+++ Pro/New folder/pro/Node/Node.ino
@@ -1,8 +1,14 @@
-//#include <DS1307.h>
-#include"RTClib.h"
-#include <Wire.h>
+#include<SoftwareSerial.h>
+#include <MyRealTimeClock.h>
 
-RTC_DS1307 rtc;
+MyRealTimeClock myRTC(6, 7, 8);
+SoftwareSerial xbee(9,10);
+int sensorInterrupt=0;
+int flowratePin=5;
+float calibrationFactor=4.5;
+int pulseCount;
+float flowRate;
+unsigned long oldTime;
 int echoPin=11;
 int  trigPin=13;
 float pingTime;
@@ -10,125 +16,75 @@ float targetDistance;
 float speedOfSound=343;//in meter per second
 float originalDist=2;
 float level;
-int sensorInterrupt=0;
-int flowRatePin=2;
-float calibrationFactor=4.5;
-int pulseCount;
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
-unsigned long oldTime;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+String data="";
 
 
 void setup() {
+  // put your setup code here, to run once:
+
   Serial.begin(9600);
+  xbee.begin(9600);
+  delay(100);
   pinMode(echoPin,INPUT);
   pinMode(trigPin,OUTPUT);
-  pinMode(flowRatePin, INPUT);
-  digitalWrite(flowRatePin, HIGH);
-
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
+  pinMode(flowratePin,INPUT);
+  digitalWrite(flowratePin,HIGH);
+  pulseCount=0;
+  flowRate=0.0;
+  oldTime=0;
+  myRTC.setDS1302Time(00, 12, 16, 12 , 9, 06, 2019);
   delay(100);
-
-  if (! rtc.begin()) {
-    //Serial.println("Couldn't find RTC");
-    while (1);
-  }
-
-  if (! rtc.isrunning()) {
-//    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-
-//  DS1307.begin();
-//  DS1307.setDate(19,1,24,0,10,53,0);
-  
   attachInterrupt(sensorInterrupt,pulseCounter,FALLING);
+
+
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  measureLevel();
+  measureFlowRate();
+  myRTC.updateTime();
+  String timeOfReading=String(myRTC.year, DEC)+'-'+String(myRTC.month, DEC)+'-'+String(myRTC.dayofmonth, DEC)+"T"+String(myRTC.hours, DEC)+":"+String(myRTC.minutes, DEC)+":"+String(myRTC.seconds, DEC);
+  
 
-  DateTime now=rtc.now();
+  data+="\"Sensor 1\":{\"flowRate\":"+(String)flowRate+",\"level\":"+(String)level;
 
-  String timeOfRecord=String(now.year(), DEC)+'/'+String(now.month(), DEC)+'/'+String(now.day(), DEC)+" "+String(now.hour(), DEC)+":"+String(now.minute(), DEC)+":"+String(now.second(), DEC);
+//  Serial.println(data);
+  int x= data.length();
+  xbee.print("<");
+  for(int i=0;i<x;i++){
+    xbee.print(data[i]);    
+  }
+  xbee.println(">");
+  data="";
 
-//  Serial.print(now.year(), DEC);
-//  Serial.print('/');
-//  Serial.print(now.month(), DEC);
-//  Serial.print('/');
-//  Serial.print(now.day(), DEC);
-//  Serial.print(" (");
-//  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-//  Serial.print(") ");
-//  Serial.print(now.hour(), DEC);
-//  Serial.print(':');
-//  Serial.print(now.minute(), DEC);
-//  Serial.print(':');
-//  Serial.print(now.second(), DEC);
-//  Serial.println();
+  delay(60000);
 
+}
+
+void measureLevel(){
   digitalWrite(trigPin,LOW);
   delayMicroseconds(2000);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(15);
   digitalWrite(trigPin,LOW);
-
   pingTime=pulseIn(echoPin,HIGH);
   pingTime=pingTime/1000000.;
-
   targetDistance=speedOfSound * pingTime;
   targetDistance=targetDistance/2.;
-
   level=originalDist - targetDistance;
-
-  
-
-//  Serial.print("The distance is: ");
-//  Serial.println(targetDistance);
-
-  
-
-  if((millis()-oldTime)>1000){
-    detachInterrupt(sensorInterrupt);
-    flowRate= ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    oldTime = millis();
-    flowMilliLitres = (flowRate / 60) * 1000;
-    totalMilliLitres += flowMilliLitres;
-    unsigned int frac;
-//    Serial.print("Flow rate: ");
-//    Serial.print(int(flowRate));  // Print the integer part of the variable
-//    Serial.print(".");             // Print the decimal point
-    frac = (flowRate - int(flowRate)) * 10;
-//    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-//    Serial.print("L/min");
-//    Serial.print("  Current Liquid Flowing: ");             // Output separator
-//    Serial.print(flowMilliLitres);
-//    Serial.print("mL/Sec");
-//    Serial.print("  Output Liquid Quantity: ");             // Output separator
-//    Serial.print(totalMilliLitres);
-//    Serial.println("mL"); 
-    pulseCount = 0;
-    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-  }
-
-  String data="\"sensor1\":{\"flowrate\":"+(String)flowRate+",\"level\":"+(String)level+",\"time\":"+timeOfRecord+"}";
-  Serial.println(data);
-  
-  delay(3800);
-
 }
 
-void pulseCounter()
-{
-  // Increment the pulse counter
+void measureFlowRate(){
+  if((millis()-oldTime)>1000){
+    detachInterrupt(sensorInterrupt);
+    flowRate=((1000.0/(millis()-oldTime))*pulseCount)/calibrationFactor;
+    oldTime=millis();
+    pulseCount=0;
+    attachInterrupt(sensorInterrupt,pulseCounter,FALLING);
+  }
+}
+
+void pulseCounter(){
   pulseCount++;
 }
